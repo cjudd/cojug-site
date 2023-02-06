bac4dc157155d1773c76ad9cc6be4db42c24fb34
Christopher M. Judd <javajudd@gmail.com>
2023-02-06 11:59:05 -0500

    Updated deployment info.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 # bundle exec jekyll build
 # bundle exec jekyll serve
 
-aws s3 sync --region us-east-2 _site s3://www.cojug.org/
+aws s3 sync --region us-east-2 --profile cjudd _site s3://www.cojug.org/
