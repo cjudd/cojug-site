4477b20b57b82a2bd4cc2cf9554ec07a7d513430
Christopher M. Judd <javajudd@gmail.com>
2023-10-31 12:37:58 -0400

    Added https by moving to cloudfront.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 # bundle exec jekyll build
 # bundle exec jekyll serve
 
-aws s3 sync --region us-east-2 --profile cjudd _site s3://www.cojug.org/
+aws s3 sync --region us-east-2 --profile cjudd _site s3://www.cojug.org/ --exclude ".git/*" --exclude "_site/*" --exclude ".idea/*" --exclude .gitignore --exclude deploy.sh --exclude *.iml
+aws cloudfront --profile cjudd create-invalidation --distribution-id E2ZHP6LW6YVY7T --paths "/*"
